26b912835519a08164466af8669f2684e823e0fc
agionoja <pauldivineagionoja@yahoo.com>
2024-06-11 02:11:17 +0100

    Feature: implemented cache control middleware

---
+++ src/middlewares/handleCache.js
@@ -0,0 +1,24 @@
+import generateEtag from "../utils/generateEtag.js";
+
+const handleCache = (req, res, next) => {
+  res.sendReponse = res.json;
+
+  res.json = (body) => {
+    const etag = generateEtag(body);
+
+    if (req.headers["if-none-match"] === etag) {
+      return res.status(304).send();
+    }
+
+    res.set({
+      "cache-control": "max-age=3600",
+      etag,
+    });
+
+    return res.sendReponse(body);
+  };
+
+  next();
+};
+
+export default handleCache;
